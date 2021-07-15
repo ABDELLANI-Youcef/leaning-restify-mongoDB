8febf2339336ed47392b9ff4a4bb191ba480173d
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-07-15 23:05:38 +0100

    add update customer method

--- routes/customers.js
+++ routes/customers.js
@@ -3,36 +3,54 @@ const Customer = require('../models/Customer')
 
 module.exports = (server) => {
   // Get Customers
-  server.get('/customers', async (req, res,next)=>{
+  server.get('/customers', async (req, res, next) => {
     try {
       const customers = await Customer.find({});
       res.send(customers);
       next();
     }
-    catch (err){
+    catch (err) {
       return next(new errors.InvalidContentError(err));
     }
   });
 
   // Get Single Customer
-  server.get('/customers/:id', async (req, res,next)=>{
+  server.get('/customers/:id', async (req, res, next) => {
     try {
       const customer = await Customer.findById(req.params.id);
       res.send(customer);
       next();
     }
-    catch (err){
+    catch (err) {
+      return next(new errors.ResourceNotFoundError(`There is no user with the id of ${req.params.id}`));
+    }
+  });
+
+  // Update Customer
+  server.put('/customers/:id', async (req, res, next) => {
+    if (!req.is('application/json')) {
+      return next(new errors.InvalidContentError("Expects 'application/josn'"));
+    }
+    try {
+      const customer = await Customer.findOneAndUpdate(
+        {
+          _id: req.params.id
+        },
+        req.body);
+      res.send(200);
+      next();
+    } catch (err) {
       return next(new errors.ResourceNotFoundError(`There is no user with the id of ${req.params.id}`));
     }
   });
 
   // Add Customer
-  server.post('/customers', async (req,res,next)=>{
-    if (!req.is('application/json')){
+  server.post('/customers', async (req, res, next) => {
+    if (!req.is('application/json')) {
       return next(new errors.InvalidContentError("Expects 'application/josn'"));
     }
-    const {name, email, balance} = req.body;
-    const customer = new Customer({name, email, balance});
+    const { name, email, balance } = req.body;
+    const customer = new Customer({ name, email, balance });
     try {
       const newCustomer = customer.save();
       res.send(201);
@@ -40,5 +58,5 @@ module.exports = (server) => {
     } catch (err) {
       return next(new errors.InternalError(err.message));
     }
-  })
+  });
 }
